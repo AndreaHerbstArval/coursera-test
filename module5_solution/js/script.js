f884737bfae6637c94076e3c72cdc55939d5cdb1
AndreaHerbstArval <51474377+AndreaHerbstArval@users.noreply.github.com>
2019-07-03 20:34:41 +0200

    fix it

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -111,16 +111,17 @@ function buildAndShowHomeHTML (categories) {
       // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
       // being passed into the $dc.loadMenuItems function. Think about what that argument needs
       // to look like. For example, a valid call would look something like this:
-      $dc.loadMenuItems(chosenCategoryShortName);
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage  = buildAndShowMenuItemsHTML(categoryMenuItems);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
